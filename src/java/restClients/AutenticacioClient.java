eb1e2f2a4b14401cb7b7c4596a03acef64ccfa6f
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-12-17 22:37:43 +0100

    master: added new functionality, now you can modify/reset password

--- src/java/restClients/AutenticacioClient.java
+++ src/java/restClients/AutenticacioClient.java
@@ -34,6 +34,11 @@ public class AutenticacioClient {
         webTarget = client.target(BASE_URI).path("autenticacio");
     }
 
+    public Response modifyPasswordOfClient(Object requestEntity) throws ClientErrorException{
+        WebTarget resource = webTarget;
+        resource = resource.path("modifyPassword");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON), Response.class);
+    }
     public Response createNewClientAutenticated(Object requestEntity) throws ClientErrorException{
         WebTarget resource =webTarget;
         resource=resource.path("newClient");
---
+++ web/modifiedUserWeb.jsp
@@ -0,0 +1,3 @@
+<% Object o = request.getAttribute("modifiedPasswdUser");
+   out.println(o);
+%>
